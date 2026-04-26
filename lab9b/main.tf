762d833df22742e10d4bd047bf151b188ddd5ae9
finkord <devfinkord@gmail.com>
2026-04-27 02:18:20 +0300

    lab10 ready, lab9(a,b,c) and lab11 templates added

---
+++ lab9b/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg9b" {
+  name     = "az104-rg9b"
+  location = "East US"
+}
+
